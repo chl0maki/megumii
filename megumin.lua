3aa5b9f2e1601857b97cd048e1733e7cb9038525
chl0maki <36380178+chl0maki@users.noreply.github.com>
2018-02-11 22:00:50 +0200

    commands
    cmds command.

--- megumin.lua
+++ megumin.lua
@@ -1,12 +1,15 @@
 -- kms
 -- also, if you came here for a token, move away because there aint any tokens.
 
+-- functions
+
 local discordia = require('discordia')
 local client = discordia.Client()
 local json = require('json')
 
 local date, time = os.date, os.time
 
+-- commands
 
 client:on('ready', function()
 	print('bitch you logged in as '.. client.user.username)
@@ -21,6 +24,12 @@ end)
 client:on('messageCreate', function(message)
 if message.content == 'prefix' then
 		message.channel:send('the prefix for me is =')
+	end
+end)
+
+client:on('messageCreate', function(message)
+if message.content == '=cmds' then
+		message.channel:send('current commands are: =sonic (must require sonic emoji), prefix and =cmds.')
 	end
 end)
 
