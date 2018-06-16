5f48e3295043e2ca45e5147a689e64283c86411f
chl0maki <36380178+chl0maki@users.noreply.github.com>
2018-06-16 22:23:03 +0300

    a few changes
    made embed for dog and cat, changed cmds. added support for more channels on the hentai command.

--- modules/stuff.lua
+++ modules/stuff.lua
@@ -7,17 +7,27 @@ local random = math.random
 local cmds = {}
 
 cmds["dog"] = function(arg, message)
-	local get, body = http.request("GET", "https://random.dog/woof.json")
-	message.channel:send(json.decode(body)["url"])
-end
+local get, body = http.request("GET", "https://random.dog/woof.json")
+        message.channel:send {
+            embed = {
+                title = "dog",
+                image = { url = json.decode(body)["url"] }
+            }
+        }
+    end
 
 cmds["cat"] = function(arg, message)
-	local get, body = http.request("GET", "http://aws.random.cat/meow")
-	message.channel:send(json.decode(body)["file"])
-end
+local get, body = http.request("GET", "https://aws.random.cat/meow")
+        message.channel:send {
+            embed = {
+                title = "cat",
+                image = { url = json.decode(body)["url"] }
+            }
+        }
+    end
 
 cmds["hentai"] = function(arg, message)
-    if message.channel.nsfw then
+    if message.channel.nsfw or message.channel.nsfw-chat or message.channel.hentai then
         local get, body = http.request("GET", "https://nekos.life/api/v2/img/lewd")
         message.channel:send {
             embed = {
@@ -34,7 +44,7 @@ cmds["coinflip"] = function(arg, message)
 end
 
 cmds["cmds"] = function(arg, message)
-	message.channel:send("current commands are: =sonic, =cat, =dog, =hentai (nsfw channel), =coinflip and =cmds.")
+	message.channel:send("current commands are: =sonic, =cat, =dog, =hentai (nsfw channel), =coinflip and =cmds. (note: check https://aws.random.cat/meow if =cat doesnt work.) if =hentai doesn't work, name your nsfw channel to nsfw, nsfw-chat, or hentai.")
 end
 
 cmds["sonic"] = function(arg, message)
